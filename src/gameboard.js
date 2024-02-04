28ba335d31866879e2d0533bf80b97c8023f5d1e
gonzalo <gonzalo.a.gomez22@gmail.com>
2024-02-04 14:14:22 -0300

    Refactor placeShip method and add receiveAttack method to register hits on ships and misses on the board

--- src/gameboard.js
+++ src/gameboard.js
@@ -6,21 +6,40 @@ class Gameboard {
     for (let i = 0; i < size; i++) {
       this.board[i] = [];
       for (let j = 0; j < size; j++) {
-        this.board[i][j] = [i, j];
+        this.board[i][j] = null;
       }
     }
   }
 
-  placeShip(ship) {
-    this.board[0][0] = [ship];
-    this.board[0][1] = [ship];
-    this.board[0][2] = [ship];
+  placeShip(ship, x, y, orientation) {
+    if (orientation === "horizontal") {
+      for (let i = 0; i < ship.length; i++) {
+        this.board[x][y + i] = ship;
+      }
+    } else if (orientation === "vertical") {
+      for (let i = 0; i < ship.length; i++) {
+        this.board[x + i][y] = ship;
+      }
+    }
+  }
+
+  receiveAttack(x, y) {
+    if (this.board[x][y] === null) {
+      this.board[x][y] = "miss";
+    } else {
+      this.board[x][y].hit();
+    }
   }
 }
-
 // const board = new Gameboard(10);
-// const destro = new Ship(3, 0, "afloat");
-
-// board.placeShip(destro);
+// const destroyer = new Ship(3, 0, "afloat");
+// board.placeShip(destroyer, 0, 0, "horizontal");
+// board.receiveAttack(0, 0);
+// board.receiveAttack(0, 1);
+// board.receiveAttack(0, 2);
+// board.receiveAttack(0, 3); //doesn't work
+// console.log(board.board[0][2]);
+// console.log(board.board[0][3]); // doesn't work
+// console.log(board.board[0][0]); // doesn't work
 
 module.exports = Gameboard;
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,8 +1,6 @@
 const Gameboard = require("../src/gameboard.js");
 const Ship = require("../src/battleship.js");
 
-// const board = new Gameboard(10);
-
 test("Gameboard returns the correct number of grids", () => {
   const board = new Gameboard(10);
 
@@ -12,10 +10,31 @@ test("Gameboard returns the correct number of grids", () => {
 
 test("Gameboard displays the ship in the correct coordinates", () => {
   const board = new Gameboard(10);
-  const destroyer = new Ship(3, 0, "afloat");
-  board.placeShip(destroyer);
+  const destroyer = new Ship(3);
+  board.placeShip(destroyer, 0, 0, "horizontal");
 
-  expect(board.board[0][0]).toStrictEqual([destroyer]);
-  expect(board.board[0][1]).toStrictEqual([destroyer]);
-  expect(board.board[0][2]).toStrictEqual([destroyer]);
+  expect(board.board[0][0]).toStrictEqual(destroyer);
+  expect(board.board[0][1]).toStrictEqual(destroyer);
+  expect(board.board[0][2]).toStrictEqual(destroyer);
+});
+
+test("Increase hit counter when a ship gets hit in the correct coordinates", () => {
+  const board = new Gameboard(10);
+  const destroyer = new Ship(3);
+  board.placeShip(destroyer, 0, 0, "horizontal");
+  board.receiveAttack(0, 0);
+  board.receiveAttack(0, 1);
+  board.receiveAttack(0, 2);
+
+  expect(destroyer.timesBeenHit).toStrictEqual(3);
+  expect(destroyer.shipStatus).toStrictEqual("sunk");
+});
+
+test("Gameboard handles missed shots correctly", () => {
+  const board = new Gameboard(10);
+  board.receiveAttack(1, 0);
+  board.receiveAttack(1, 1);
+
+  expect(board.board[1][0]).toStrictEqual("miss");
+  expect(board.board[1][1]).toStrictEqual("miss");
 });
