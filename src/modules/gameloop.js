ec39c6962235f3be80ec32026f5e5cbd6b630dd3
gonzalo <gonzalo.a.gomez22@gmail.com>
2024-03-31 15:52:36 -0300

    Add main method to initialize the game. Add method to switch turns

--- src/modules/gameloop.js
+++ src/modules/gameloop.js
@@ -1,29 +1,55 @@
-import DOM from "./DOMinteraction.js";
 import Player from "./player.js";
-import Ship from "./ship.js";
 
 export default function gameloop() {
-  const battleship = new Ship(4, 0, "afloat");
-  const destroyer = new Ship(3, 0, "afloat");
-  const submarine = new Ship(3, 0, "afloat");
-  const boat1 = new Ship(2, 0, "afloat");
-  const boat2 = new Ship(2, 0, "afloat");
-
   const player1 = new Player("User");
   const player2 = new Player("CPU");
 
-  player1.gameboard.placeShip(battleship, 0, 0, "horizontal");
-  player1.gameboard.placeShip(destroyer, 1, 0, "horizontal");
-  player1.gameboard.placeShip(submarine, 2, 0, "horizontal");
-  player1.gameboard.placeShip(boat1, 3, 0, "horizontal");
-  player1.gameboard.placeShip(boat2, 4, 0, "horizontal");
+  let currentPlayer = player1;
+  let nextPlayer = player2;
+  let gameOver = false;
 
-  player2.gameboard.placeShip(battleship, 5, 0, "horizontal");
-  player2.gameboard.placeShip(destroyer, 6, 0, "horizontal");
-  player2.gameboard.placeShip(submarine, 7, 0, "horizontal");
-  player2.gameboard.placeShip(boat1, 8, 0, "horizontal");
-  player2.gameboard.placeShip(boat2, 9, 0, "horizontal");
-  console.log(player1.gameboard);
-  console.log(player2.gameboard);
-  DOM();
+  player1.placeShipManually("battleship", 4, 0, 0, "horizontal");
+  player1.placeShipManually("sub", 3, 1, 0, "horizontal");
+  player1.placeShipManually("destroyer", 3, 2, 0, "horizontal");
+  player1.placeShipManually("boat1", 2, 4, 0, "horizontal");
+  player1.placeShipManually("boat2", 2, 5, 0, "horizontal");
+
+  player2.placeShipManually("battleship", 4, 0, 0, "horizontal");
+  player2.placeShipManually("destroyer", 3, 1, 0, "horizontal");
+  player2.placeShipManually("sub", 3, 2, 0, "horizontal");
+  player2.placeShipManually("boat1", 2, 3, 0, "horizontal");
+  player2.placeShipManually("boat2", 2, 4, 0, "horizontal");
+
+  function switchTurn() {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+    nextPlayer = currentPlayer === player1 ? player2 : player1;
+  }
+
+  function play() {
+    while (!gameOver) {
+      console.log(`Current player: ${currentPlayer.player}`);
+
+      // Get player move
+      const [x, y] = nextPlayer.cpuPlay();
+
+      // Check if the move hits a ship
+      if (nextPlayer.gameboard.board[x][y] !== "miss") {
+        // console.log("Hit!");
+
+        // Check if all opponent's ships are sunk
+        if (nextPlayer.gameboard.sunkShips()) {
+          console.log(player1.gameboard.ships);
+          console.log(player2.gameboard.ships);
+          console.log(`${currentPlayer.player} wins!`);
+          gameOver = true;
+        }
+      } else {
+        // console.log("Miss!");
+      }
+      switchTurn();
+    }
+  }
+  play();
 }
+
+gameloop();
